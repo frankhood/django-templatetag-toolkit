f6c2e8759e3542e5da86668aefa1e8021bae574f
Davide Ria <d.ria@frankhood.it>
2021-09-29 13:12:18 +0200

    FRANK-793 Removed django- from this package

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 requirements = open('requirements.txt').readlines()
 
 setup(
-    name='django-templatetag-toolkit',
+    name='templatetag-toolkit',
     version=version,
     description="""Your project description goes here""",
     long_description=readme + '\n\n' + history,
